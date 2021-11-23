cfcabfd82a212e4b6a373a031dcd0e7d45d537e6
stalonam <hmasteraaa@gmail.com>
2021-11-23 13:03:01 +0200

    Initial Commit

---
+++ ICHAT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ICHAT
+//
+//  Created by Antbook on 23.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
